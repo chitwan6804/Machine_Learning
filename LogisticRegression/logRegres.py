49014489a57c5a16ec7b4d4e29893d09a51c2530
Chitwan <128251060+chitwan6804@users.noreply.github.com>
2024-06-05 03:30:39 +0530

    Update logRegres.py

--- LogisticRegression/logRegres.py
+++ LogisticRegression/logRegres.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 def loadDataSet():
     dataMat = []
@@ -29,12 +31,11 @@ def gradAscent(dataMatIn, classLabels):
         weights = weights + alpha * dataMatrix.transpose() * error
     return weights
 
-weights = gradAscent(dataArr, labelMat)
-print(weights)
+# to test gradAscent
+# weights = gradAscent(dataArr, labelMat)
+# print(weights)
 
 def plotBestFit(wei):
-    import matplotlib.pyplot as plt
-    weights = wei.getA()
     dataMat, labelMat = loadDataSet()
     dataArr = np.array(dataMat)
     n = np.shape(dataArr)[0] 
@@ -52,10 +53,37 @@ def plotBestFit(wei):
     ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
     ax.scatter(xcord2, ycord2, s=30, c='green')
     x = np.arange(-3.0, 3.0, 0.1)
-    y = (-weights[0] - weights[1] * x) / weights[2]
-    ax.plot(x, y)
+    y = (-wei[0] - wei[1] * x) / wei[2]
+    ax.plot(x, y.ravel())  # Use .ravel() to flatten y
     plt.xlabel('X1')
     plt.ylabel('X2')
     plt.show()
 
+def stochGradAscent0(dataMatrix, classLabels):
+    m, n = np.shape(dataMatrix)
+    alpha = 0.1
+    weights = np.ones(n)
+    for i in range(m):
+        h = sigmoid(np.sum(dataMatrix[i] * weights))
+        error = classLabels[i] - h
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+def stochGradAscent1(dataMatrix, classLabels, numIter=150):
+    m, n = np.shape(dataMatrix)
+    weights=np.ones(n)
+    for j in range(numIter):
+        dataIndex=list(range(m))
+        for i in range(m):
+            alpha=4/(1.0+j+i)+0.01
+            randIndex=int(random.uniform(0,len(dataIndex)))
+            h=sigmoid(np.sum(dataMatrix[randIndex] * weights))
+            error=classLabels[randIndex]-h
+            weights+=alpha*error*dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+        return weights
+
+dataArr, labelMat = loadDataSet()
+weights = stochGradAscent1(np.array(dataArr), labelMat)
+print(weights)
 plotBestFit(weights)
